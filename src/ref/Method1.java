744d2111a1712c2b9a68fad5bfabfc91d0a3bf48
김선우 <frod5@kakao.com>
2024-02-15 18:07:24 +0900

    기본형과 참조형 실습

---
+++ src/ref/Method1.java
@@ -0,0 +1,27 @@
+package ref;
+
+public class Method1 {
+	public static void main(String[] args) {
+
+		printStudent(createStudent("학생1", 15, 90));
+		printStudent(createStudent("학생2", 16, 80));
+	}
+
+	public static Student createStudent(String name, int age, int grade) {
+		Student student = new Student();
+		student.name = name;
+		student.age = age;
+		student.grade = grade;
+		return student;
+	}
+
+	public static void initStudent(Student student, String name, int age, int grade) {
+		student.name = name;
+		student.age = age;
+		student.grade = grade;
+	}
+
+	public static void printStudent(Student student) {
+		System.out.println("이름 : " + student.name + " 나이: " + student.age + " 성적: " + student.grade);
+	}
+}
